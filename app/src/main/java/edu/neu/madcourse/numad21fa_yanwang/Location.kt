0f596c66a805d72854ae74d2f460b34ad849a63b
wangyan66 <819156618@qq.com>
2021-10-10 00:01:18 -0700

    add location function

--- app/src/main/java/edu/neu/madcourse/numad21fa_yanwang/Location.kt
+++ app/src/main/java/edu/neu/madcourse/numad21fa_yanwang/Location.kt
@@ -1,11 +1,95 @@
 package edu.neu.madcourse.numad21fa_yanwang
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.material.snackbar.Snackbar
 
+private const val REQUEST_FOREGROUND_ONLY_PERMISSIONS_REQUEST_CODE = 34
+private const val TAG = "Location"
 class Location : AppCompatActivity() {
+
+    private lateinit var fusedLocationProviderClient : FusedLocationProviderClient
+    private lateinit var outputTextView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_location)
+        outputTextView = findViewById(R.id.Location_textView)
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+
+        if(checkPermission()){
+            getLocation()
+        }else{
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                REQUEST_FOREGROUND_ONLY_PERMISSIONS_REQUEST_CODE)
+        }
+
     }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+
+        when (requestCode) {
+            REQUEST_FOREGROUND_ONLY_PERMISSIONS_REQUEST_CODE -> when {
+                grantResults.isEmpty() ->
+                    // If user interaction was interrupted, the permission request
+                    // is cancelled and you receive empty arrays.
+                    Log.d(TAG, "User interaction was cancelled.")
+                grantResults[0] == PackageManager.PERMISSION_GRANTED ->
+                    // Permission was granted.
+                    getLocation()
+
+                else -> {
+                    // Permission denied.
+
+                    Snackbar.make(
+                        findViewById(R.id.location_activity),
+                        R.string.permission_denied_explanation,
+                        Snackbar.LENGTH_LONG
+                    )
+                        .setAction(R.string.settings) {
+                            // Build intent that displays the App settings screen.
+                            val intent = Intent()
+                            intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+                            val uri = Uri.fromParts(
+                                "package",
+                                BuildConfig.APPLICATION_ID,
+                                null
+                            )
+                            intent.data = uri
+                            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                            startActivity(intent)
+                        }
+                        .show()
+                }
+            }
+        }
+    }
+    private fun checkPermission() : Boolean{
+        return PackageManager.PERMISSION_GRANTED == ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+    }
+    private fun getLocation() {
+        try {
+            fusedLocationProviderClient.lastLocation.addOnSuccessListener {
+                outputTextView.text = "latitude: " + it.latitude + "\n" + "longitude: " + it.longitude
+            }
+        }catch (unlikely: SecurityException) {
+            Toast.makeText(this, "Lost location permissions. Couldn't remove updates.", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
 }
